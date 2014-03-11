eb4ad3a2a4eb31595f15c1f0ab34bd675719733c
panjunyong <panjunyong@everydo.com>
2014-03-11 18:03:20 +0800

    change version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.6.2'
+__version__ = '0.6.3'
 
 import os
 from setuptools import setup, find_packages
